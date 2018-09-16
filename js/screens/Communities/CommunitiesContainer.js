d6c3d6ccaf00ee1cf27db43526afa64622e55144
justin <justinvyang@gmail.com>
2018-09-16 12:52:05 -0700

    fix reddit urls

--- js/screens/Communities/CommunitiesContainer.js
+++ js/screens/Communities/CommunitiesContainer.js
@@ -8,9 +8,9 @@ export default class CommunitiesContainer extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = {links:[]};
+    this.state = { links: [] };
   }
-  
+
   componentWillMount() {
     this.fetchReddit('/r/alcoholism/hot/');
     this.fetchReddit('/r/stopdrinking/hot/');
@@ -18,33 +18,33 @@ export default class CommunitiesContainer extends Component {
     this.fetchReddit('/r/alcoholicsanonymous/hot/');
     this.fetchReddit('/r/AlAnon/hot/');
     this.fetchReddit('/r/SMARTRecovery/hot/');
-  } 
-  
+  }
+
   fetchReddit = (thread) => {
-    fetch(REDDIT_URL + thread +'.json?limit=5')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      let currentLinks = this.state.links;
-      let children = responseJson.data.children;
-      let item = children.find(child => !child.data.stickied);
-      let link = {
-        url: thread,
-        subreddit: thread.substring(0,thread.length-4),
-        title: item.data.title,
-        imageurl: item.data.thumbnail === "self" || !item.data.thumbnail ? 'http://1000logos.net/wp-content/uploads/2017/05/Reddit-logo.png' : item.data.thumbnail,
-        description: item.data.selftext.length > 400 ? item.data.selftext.substring(0,399)+'...' : item.data.selftext
-      };
-      currentLinks.push(link);
-      this.setState({links:currentLinks});
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    fetch(REDDIT_URL + thread + '.json?limit=5')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let currentLinks = this.state.links;
+        let children = responseJson.data.children;
+        let item = children.find(child => !child.data.stickied);
+        let link = {
+          url: REDDIT_URL + thread,
+          subreddit: thread.substring(0, thread.length - 4),
+          title: item.data.title,
+          imageurl: item.data.thumbnail === "self" || !item.data.thumbnail ? 'http://1000logos.net/wp-content/uploads/2017/05/Reddit-logo.png' : item.data.thumbnail,
+          description: item.data.selftext.length > 400 ? item.data.selftext.substring(0, 399) + '...' : item.data.selftext
+        };
+        currentLinks.push(link);
+        this.setState({ links: currentLinks });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
     return (
-      <Communities links={this.state.links}/>
+      <Communities links={this.state.links} />
     );
   }
 }
