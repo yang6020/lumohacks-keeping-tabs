562d624b8bfca88cded99c318ec36036666c5aa4
Andrei Egorov <andreyegorov1992@gmail.com>
2018-09-15 23:24:06 -0700

    fix merge conflict

--- js/App.js
+++ js/App.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import { ApolloProvider } from "react-apollo";
-
-import React, { Component } from 'react';
-import RootStackNavigator from "./navigation/RootStackNavigation";
-import client from "./config/api";
-=======
 import React, { Component } from 'react';
 import client from './config/api';
 import { ApolloProvider } from 'react-apollo';
 import createRootNavigator from './navigation/RootStackNavigation';
 import { getUser } from './config/models';
->>>>>>> finish AccountForm add login-signup-logout functionality with validation
 
 export default class App extends Component {
   constructor(props) {
@@ -30,11 +22,7 @@ export default class App extends Component {
     const Layout = createRootNavigator(this.state.signedIn);
     return (
       <ApolloProvider client={client}>
-<<<<<<< HEAD
-      <RootStackNavigator />
-=======
         <Layout />
->>>>>>> finish AccountForm add login-signup-logout functionality with validation
       </ApolloProvider>
     );
   }
