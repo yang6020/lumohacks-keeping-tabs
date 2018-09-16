09ed1896519499825e0ab90aa2e9391acc3a2f6c
ychan <oyves.chan@ca.com>
2018-09-16 01:51:38 -0700

    just pull from google..

--- js/screens/Resources/ResourcesContainer.js
+++ js/screens/Resources/ResourcesContainer.js
@@ -26,8 +26,37 @@ export default class ResourcesContainer extends Component {
   static navigationOptions = {
     title: 'Resources',
   };
+  constructor(props) {
+  super(props);
+  this.state = {links:[]};
+}
+
+componentWillMount() {
+  //TODO: put this vars in a config file
+  let GOOGLE_SEARCH_KEY='AIzaSyCNEAFL_8DTUaEk2FwhiI3xOJz4xl3_QlU'
+  let GOOGLE_SEARCH_ENGINE='000456785991468921886:zdbnnimdfaw'
+  let GRAPH_COOL_URL='https://api.graph.cool/simple/v1/cjm49ftxt4kgu0100984ridlk'
+  
+  fetch('https://www.googleapis.com/customsearch/v1?key='+GOOGLE_SEARCH_KEY+'&cx='+GOOGLE_SEARCH_ENGINE+'&q=alcohol+addiction')
+  .then((response) => response.json())
+  .then((responseJson) => {
+    let items = responseJson.items;
+    items.map(item => {
+      return {
+        url: item.pagemap.metatags[0].['og:url']
+        title: item.pagemap.metatags[0].['og:title']
+        imageurl: item.pagemap.metatags[0].['og:image'],
+        description: item.pagemap.metatags[0].['og:description']
+      }
+    });
+    this.setState({links:items});
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
+  
   render() {
-    //TODO: return graph QL for dummy
-    return <Resources links={dummy}/>;
+    return <Resources links={this.state.links}/>;
   }
 }
