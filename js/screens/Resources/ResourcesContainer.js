ed103d2ff9c09893bc7a99780abdb7c7fd0cb274
ychan <oyves.chan@ca.com>
2018-09-16 02:49:43 -0700

    googlers

--- js/screens/Resources/ResourcesContainer.js
+++ js/screens/Resources/ResourcesContainer.js
@@ -35,19 +35,18 @@ componentWillMount() {
   //TODO: put this vars in a config file
   let GOOGLE_SEARCH_KEY='AIzaSyCNEAFL_8DTUaEk2FwhiI3xOJz4xl3_QlU'
   let GOOGLE_SEARCH_ENGINE='000456785991468921886:zdbnnimdfaw'
-  let GRAPH_COOL_URL='https://api.graph.cool/simple/v1/cjm49ftxt4kgu0100984ridlk'
   
   fetch('https://www.googleapis.com/customsearch/v1?key='+GOOGLE_SEARCH_KEY+'&cx='+GOOGLE_SEARCH_ENGINE+'&q=alcohol+addiction')
   .then((response) => response.json())
   .then((responseJson) => {
-    let items = responseJson.items;
-    items.map(item => {
+    let items = responseJson.items.map(item => {
+
       return {
-        url: item.pagemap.metatags[0].['og:url']
-        title: item.pagemap.metatags[0].['og:title']
-        imageurl: item.pagemap.metatags[0].['og:image'],
-        description: item.pagemap.metatags[0].['og:description']
-      }
+        url: item.pagemap.metatags[0]['og:url'] ? item.pagemap.metatags[0]['og:url'] : '',
+        title: item.pagemap.metatags[0]['og:title'] ? item.pagemap.metatags[0]['og:title'] : '',
+        imageurl: item.pagemap.metatags[0]['og:image'] ? item.pagemap.metatags[0]['og:image'] : 'https://i.cbc.ca/1.4172149.1498079824!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/201472-467835623js001-burgeoning-c.jpg',
+        description: item.pagemap.metatags[0]['og:description'] ? item.pagemap.metatags[0]['og:description'] : ''
+      };
     });
     this.setState({links:items});
   })
