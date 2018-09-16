531c094447db32cf54f8c62f771805f814b853e6
Vincent Chan <v.chan36@gmail.com>
2018-09-15 23:20:05 -0700

    add calendar widget to calendar

--- js/config/api.js
+++ js/config/api.js
@@ -1,13 +1,7 @@
 import ApolloClient from "apollo-boost";
-<<<<<<< HEAD
 
 const client = new ApolloClient({
   uri: "https://api.graph.cool/simple/v1/cjm49ftxt4kgu0100984ridlk"
 });
-=======
-const client = new ApolloClient({
-  uri: "https://api.graph.cool/simple/v1/cjm49ftxt4kgu0100984ridlk"
-});
 
-export default client
->>>>>>> finish AccountForm add login-signup-logout functionality with validation
+export default client;
